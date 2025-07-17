efc78a343e6e09a88b0c08eafa47ebd239b26b2f
DaneDTDat <149254840+DaneDTDat@users.noreply.github.com>
2025-07-17 22:42:51 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: `https://clauddeshop-backend-production.up.railway.app//api'`,
-  baseUrl: `https://clauddeshop-backend-production.up.railway.app/`,
+  apiUrl: `https://clauddeshop-backend-production.up.railway.app/api'`,
+  baseUrl: `https://clauddeshop-backend-production.up.railway.app`,
 };
